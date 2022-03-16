e60e996ef6ed5a41cba8241ed2485465674fac51
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-16 10:38:29 -0700

    code

--- server.js
+++ server.js
@@ -5,22 +5,51 @@ const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
 
-app.get('/contact', (req, res) => {
-  res.sendFile(join(__dirname, 'public', 'contact.html'))
-})
+// app.get('/contact', (req, res) => {
+//   res.sendFile(join(__dirname, 'public', 'contact.html'))
+// })
 
-app.get('/test', (req, res) => {
-  res.send('<button>It is working!</button>')
-})
+// app.get('/test', (req, res) => {
+//   res.send('<button>It is working!</button>')
+// })
 
-const person = {
-  name: 'John Doe',
-  age: 40,
-  email: 'johndoe@gmail.com'
-}
+// const person = {
+//   name: 'John Doe',
+//   age: 40,
+//   email: 'johndoe@gmail.com'
+// }
 
-app.get('/person', (req, res) => {
-  res.json(person)
+// app.get('/person', (req, res) => {
+//   res.json(person)
+// })
+
+const roles = [
+  {
+    title: 'Boss',
+    salary: 500000
+  },
+  {
+    title: 'Manager',
+    salary: 100000
+  },
+  {
+    title: 'Employee',
+    salary: 60000
+  },
+  {
+    title: 'Assistant',
+    salary: 30000
+  },
+  {
+    title: 'Intern',
+    salary: 0
+  }
+]
+
+// role -> boss, manager, employee, intern
+app.get('/position/:role', (req, res) => {
+  const position = roles.filter(role => role.title.toLowerCase() === req.params.role.toLowerCase())[0]
+  res.json(position)
 })
 
 app.listen(3000)
